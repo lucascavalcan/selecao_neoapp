60e458d117be477a47b14d2514a00e921f835f03
Lucas Cavalcanti de Araujo <lcavalcantidearaujo@gmail.com>
2022-10-07 00:15:01 -0300

    Criação do footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,49 @@
 import * as C from "./style";
+import Logo from "../../images/logo-footer.png";
+import Image from "../../images/banner7.jpg";
 
 export const Footer = () => {
     return (
         <C.Container>
-            
+            <C.FooterTop>
+                <C.FooterLogo>
+                    <img src={Logo} width="90" height="100" alt=""/>
+                </C.FooterLogo>
+
+                <C.FooterLinks>
+
+                        <a href="https://www.marvel.com/corporate/about">SOBRE A MARVEL</a>
+                        <a href="https://www.disneyplus.com/brand/marvel?cid=DTCI-Synergy-Marvel-Site-Acquisition-Library-US-Marvel-NA-EN-NavFooter-Marvel_DisneyPlus_NavFooter_Evergreen-NA">DISNEY+</a>
+                        <a href="https://www.marvelhq.com/">MARVELHQ.COM</a>
+
+                </C.FooterLinks>
+
+                <C.FooterSocial>
+                    SIGA-NOS!
+                    <C.SocialLinks>
+                        <a href="https://twitter.com/marvel">TWITTER</a>
+                        <a href="https://www.instagram.com/marvel/">INSTAGRAM</a>
+                        <a href="https://www.youtube.com/marvel">YOUTUBE</a>
+                        <a href="https://www.facebook.com/MarvelBR/?brand_redir=6883542487">FACEBOOK</a>
+                    </C.SocialLinks>
+
+                </C.FooterSocial>
+
+                <C.FooterImage>
+                    <img src={Image} width="500"/>
+                </C.FooterImage>
+
+            </C.FooterTop>
+
+            <C.FooterBottom>
+                <C.FotterBotomLeft>
+                    © <strong>MARVEL</strong> Todos os direitos reservados.
+                </C.FotterBotomLeft>
+
+                <C.FooterBottomRight>
+                    Feito com ❤️ Criado por Lucas Cavalcanti
+                </C.FooterBottomRight>
+            </C.FooterBottom>
         </C.Container>
     )
 }
--- src/components/Footer/style.ts
+++ src/components/Footer/style.ts
@@ -1,5 +1,70 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    
+    background-color: #131a1a;
+    height: 250px;
+    margin-top: 100px;
+    display: flex;
+    flex-direction: column;
+    color: #eee;
+`;
+
+export const FooterTop = styled.div`
+    height: 210px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-around;
+`;
+
+export const FooterLogo = styled.div`
+
+`;
+
+export const FooterLinks = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    a {
+        text-decoration: none;
+        color: #fff;
+        font-weight: bold;
+        margin-bottom: 10px;
+        transition: all 0.3s ease;
+
+        &:hover {
+            color: #777;
+        }
+    }
+`;
+
+export const FooterImage = styled.div`
+    margin-top: -200px;
+    margin-right: -60px;
+`;
+
+export const FooterSocial = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+export const SocialLinks = styled.div`
+
+`;
+
+export const FooterBottom = styled.div`
+    height: 40px;
+    color: #555;
+    display:flex;
+    flex-direction: row;
+    justify-content: space-around;
+`;
+
+export const FotterBotomLeft = styled.div`
+    margin-left: -290px;
+`;
+
+export const FooterBottomRight = styled.div`
+    margin-right: -280px;
+    font-size: 14px;
 `;
